6efa1ae1948bf6bf2b16a3d0002985e13dd7d7b8
Fausto Tomei <ftomei@arpae.it>
2020-08-13 16:25:21 +0200

    update deploy

--- deploy/CRITERIA1D_build_Win_MSVC.bat
+++ deploy/CRITERIA1D_build_Win_MSVC.bat
@@ -13,32 +13,27 @@ nmake /S /NOLOGO release
 
 :: build criteriaOutput
 cd ..\Makeall_CriteriaOutput
-qmake 
-nmake /S /NOLOGO distclean
 qmake
+nmake /S /NOLOGO clean
 nmake /S /NOLOGO release
 
 :: build CRITERIA-1D
 cd ..\..\bin\Makeall_CRITERIA1D
-qmake 
-nmake /S /NOLOGO distclean
 qmake
+nmake /S /NOLOGO clean
 nmake /S /NOLOGO release
 
 :: build CROP_EDITOR
 cd ..\Makeall_CROP_EDITOR
-qmake 
-nmake /S /NOLOGO distclean
 qmake
+nmake /S /NOLOGO clean
 nmake /S /NOLOGO release
 
 :: build SOIL_EDITOR
 cd ..\Makeall_SOIL_EDITOR
-qmake 
-nmake /S /NOLOGO distclean
 qmake
+nmake /S /NOLOGO clean
 nmake /S /NOLOGO release
-
 
 :: copy executables
 cd ..\..\deploy
@@ -50,8 +45,24 @@ copy ..\..\..\bin\SOIL_EDITOR\release\SOIL_EDITOR.exe
 copy ..\..\..\tools\criteriaOutputTools\release\CriteriaOutput.exe
 copy ..\..\..\tools\csvToMeteoDb\release\CsvToMeteoDb.exe
 
+:: CLEAN distribution
+cd ..\..\..\tools\csvToMeteoDb
+nmake /S /NOLOGO distclean
+
+cd ..\Makeall_CriteriaOutput
+nmake /S /NOLOGO distclean
+
+cd ..\..\bin\Makeall_CRITERIA1D
+nmake /S /NOLOGO distclean
+
+cd ..\Makeall_CROP_EDITOR
+nmake /S /NOLOGO distclean
+
+cd ..\Makeall_SOIL_EDITOR
+nmake /S /NOLOGO distclean
 
 :: deploy executables
+cd ..\..\deploy\CRITERIA1D\bin
 windeployqt CsvToMeteoDb.exe
 windeployqt CriteriaOutput.exe
 windeployqt CRITERIA1D.exe
@@ -81,3 +92,4 @@ xcopy /s /Y ..\..\..\..\..\DATA\PROJECT\kiwifruit\*.*
 
 :: return to deploy directory
 cd ..\..\..\..\
+
