f27e259207dfde1d588ec9aa636d113eeb10d863
lauracosta <lauramail@iol.it>
2020-08-24 17:56:31 +0200

    main

--- tools/criteriaOutputTools/main.cpp
+++ tools/criteriaOutputTools/main.cpp
@@ -27,9 +27,7 @@ int main(int argc, char *argv[])
         #ifdef TEST
                 if (! searchDataPath(&myProject.dataPath)) return -1;
 
-                //settingsFileName = myProject.dataPath + "PROJECT/INCOLTO_new/bollAgro.ini";
-                settingsFileName = myProject.dataPath + "PROJECT/INCOLTO_new/nitrati.ini";
-                //settingsFileName = myProject.dataPath + "PROJECT/INCOLTO/bollAgro_cut.ini";
+                settingsFileName = myProject.dataPath + "PROJECT/INCOLTO/nitrati.ini";
                 dateComputationStr = "2020-08-13";
                 //dateComputationStr = QDateTime::currentDateTime().date().toString("yyyy-MM-dd");
                 operation = "AGGREGATION";
