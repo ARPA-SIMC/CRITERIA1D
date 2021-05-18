21adb1d2b5fade602aff5248844f5c66861ff26f
Fausto Tomei <ftomei@arpae.it>
2021-05-18 15:20:15 +0200

    update

--- bin/CRITERIA1D/main.cpp
+++ bin/CRITERIA1D/main.cpp
@@ -19,10 +19,11 @@ void usage()
               << "\nUsage:" << std::endl
               << "CRITERIA1D <project.ini> [lastDate]" << std::endl
               << "CRITERIA1D <project.ini> [firstDate] [lastDate]" << std::endl
-              << "\ndate must be in YYYY-MM-DD format" << std::endl
-              << "Point projects (SQLite): default are the first and last date in the meteo table" << std::endl
-              << "Grid projects (MySQL): default lastDate is yesterday" << std::endl
-              << "firstDate and lastDate can be also defined in project.ini" << std::endl << std::endl;
+              << "\nNotes:" << std::endl
+              << "date must be in YYYY-MM-DD format" << std::endl
+              << "firstDate and lastDate can be also defined in the project.ini" << std::endl
+              << "default lastDate in the grid projects (MySQL) is yesterday" << std::endl
+              << "default dates in the point projects (SQLite) are the first and last date in the meteo table" << std::endl;
 }
 
 
