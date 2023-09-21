f77a4e18669fe2714a680abacbb384c709270ed3
ftomei <ftomei@arpae.it>
2023-09-21 17:04:11 +0200

    fix monthly statistics

--- bin/CRITERIA1D/main.cpp
+++ bin/CRITERIA1D/main.cpp
@@ -8,7 +8,7 @@
 #include "utilities.h"
 
 // uncomment to execute test
-//#define TEST
+#define TEST
 //#define TEST_GEO
 //#define TEST_FIRSTRUN
 //#define TEST_RESTART
@@ -54,6 +54,7 @@ int main(int argc, char *argv[])
         #ifdef TEST
             //projectFileName = projectPath + "kiwifruit/kiwifruit.ini";
             projectFileName = projectPath + "montue/montue.ini";
+            projectFileName = "//moses-arpae/CRITERIA1D/PROJECTS/CLARA/monthlyClimate/C4_June_climate.ini";
         #else
             #ifdef TEST_GEO
                 //projectFileName = projectPath + "INCOLTO/incolto.ini";
